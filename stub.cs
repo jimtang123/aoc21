aca55bc30e9fab17b0663c16c1642cfce6d59dbc
Jim Tang <jim.jim.tang@hotmail.com>
2021-12-12 16:03:46 +1100

    Use file scoped namespaces

--- stub.cs
+++ stub.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using Utils;
 
-namespace Aoc21
+namespace Aoc21;
+
+public static class DayDAY_NUMBER
 {
-    public static class DayDAY_NUMBER
+    public static string? Run(string whichPart)
     {
-        public static string? Run(string whichPart)
+        return whichPart switch
         {
-            return whichPart switch
-            {
-                "a" => PartA(),
-                "b" => PartB(),
-                _   => null
-            };
-        }
+            "a" => PartA(),
+            "b" => PartB(),
+            _   => null
+        };
+    }
 
-        static string PartA()
-        {
-            var f = Utils.Utils.OpenInput("DAY_NUMBER");
-        }
+    static string PartA()
+    {
+        var f = Utils.Utils.OpenInput("DAY_NUMBER");
+    }
 
-        static string PartB()
-        {
-            var f = Utils.Utils.OpenInput("DAY_NUMBER");
-        }
+    static string PartB()
+    {
+        var f = Utils.Utils.OpenInput("DAY_NUMBER");
     }
 }
